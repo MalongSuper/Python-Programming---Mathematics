ff188ccf4278f3337950c667d7423e340d848c0c
MalongSuper <nguyenlong19082005@gmail.com>
2025-09-08 17:43:31 +0700

    Greatest Common Divisor

---
+++ PrimeFactorizationMethod.py
@@ -0,0 +1,44 @@
+# Prime Factorization Method for Greatest Common Divisor
+# For two integers
+from math import sqrt
+
+
+def prime_factors(n):  # Find all the prime factors of the numbers
+    factors = []
+    for i in range(2, n + 1):
+        isPrime = True  # reset here
+        for p in range(2, int(sqrt(i)) + 1):
+            if i % p == 0:
+                # If true, the number is not prime
+                isPrime = False
+                break
+        # If the first number is prime and is divided to the input number
+        if isPrime and (n % i) == 0:
+            # Use a loop to divide the number
+            while (n % i) == 0:
+                # Display the numbers that are prime factors (can be repeated)
+                factors.append(i)
+                # Continue the loop until it can no longer divide
+                n //= i
+    return factors
+
+
+print("Greatest Common Divisor - Prime Factorization Method")
+a = int(input("Enter first integer: "))
+b = int(input("Enter second integer: "))
+# Find all similar prime factors, including duplicate
+pa, pb = prime_factors(a), prime_factors(b)  # Return as a list
+print("Prime Factors of", a, "are", pa)
+print("Prime Factors of", b, "are", pb)
+# Step 1: Find the factors that are common in both lists
+# By intersection
+common_factors = list(set(pa) & set(pb))
+print("Common Factors", common_factors)
+# Step 2: Find the occurrence of each of the common factors
+# In both lists, and then takes the minimum one
+# Use the values to power the common factors
+gcd = 1
+for c in common_factors:
+    gcd *= c ** min(pa.count(c), pb.count(c))
+
+print("GCD is", gcd)
